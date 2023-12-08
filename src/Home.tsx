e4518efc19be60bc86350bae53aaf59b659e2cde
tkchan212 <crimsonchan@outlook.com>
2023-12-08 17:32:44 +0800

    home responsive layout(IP)

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import './index.css'
 import { ExploreButton, Navigation, Typography } from './DesignSystem'
+import { useMediaQuery } from 'usehooks-ts'
 
 function Home() {
+  const isDesktop = useMediaQuery('(min-width: 700px)')
   return (
     <div className='home' style={{
       minHeight: "100vh",
@@ -12,15 +14,22 @@ function Home() {
     }}>
       <div className='primary-header flex'>{/* we're working in react so cannot use the header anymore */}
         <div className='logo'>{/* TODO put in the logo when you have the asset files. */}</div>
-      <Navigation items={[
-        { number: '00', text: 'Home'},
-        { number: '01', text: 'Destination' },
-        { number: '02', text: 'Crew' },
-        { number: '03', text: 'Technology' },
-      ]} activeItem="00" />
+        {
+          isDesktop ?
+          <Navigation items={[
+            { number: '00', text: 'Home'},
+            { number: '01', text: 'Destination' },
+            { number: '02', text: 'Crew' },
+            { number: '03', text: 'Technology' },
+          ]} activeItem="00" />
+          : null
+        }
+
       </div>
-      
-      <div className='grid-container'>
+      {
+        isDesktop ? 
+        (
+          <div className='grid-container'>
         <div className='grid-div-1'>
       <Typography variant={"h5"}>So, you want to travel to</Typography>
       <Typography variant={"h1"}>Space</Typography>
@@ -32,6 +41,22 @@ function Home() {
       </div>
       <div className='grid-div-2'><ExploreButton /></div>
       </div>
+        )
+        : (
+          <div className='flow align-center text-center'>
+            <Typography variant={"h5"}>So, you want to travel to</Typography>
+            <Typography variant={"h1"}>Space</Typography>
+            <p className='text-accent'>
+            Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience! 
+            </p> 
+            <ExploreButton />
+          </div>
+        )
+      }
+      
+      
 
      
       
